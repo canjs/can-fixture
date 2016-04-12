c1f7db9a82a19800809e4bd1b439d4dee5d154ec
Justin Meyer <justinbmeyer@gmail.com>
2016-04-11 20:23:22 -0500

    0.2.1

---
+++ dist/amd/helpers/legacyStore.js
@@ -0,0 +1,180 @@
+/*helpers/legacyStore*/
+define(function (require, exports, module) {
+    var helpers = require('./helpers');
+    var getId = require('./getid');
+    var canSet = require('can-set');
+    module.exports = function (count, make, filter) {
+        var currentId = 0, items, findOne = function (id) {
+                for (var i = 0; i < items.length; i++) {
+                    if (id == items[i].id) {
+                        return items[i];
+                    }
+                }
+            }, methods = {}, types, reset;
+        if (helpers.isArrayLike(count) && typeof count[0] === 'string') {
+            types = count;
+            count = make;
+            make = filter;
+            filter = arguments[3];
+        } else if (typeof count === 'string') {
+            types = [
+                count + 's',
+                count
+            ];
+            count = make;
+            make = filter;
+            filter = arguments[3];
+        }
+        if (typeof count === 'number') {
+            items = [];
+            reset = function () {
+                items = [];
+                for (var i = 0; i < count; i++) {
+                    var item = make(i, items);
+                    if (!item.id) {
+                        item.id = i;
+                    }
+                    currentId = Math.max(item.id + 1, currentId + 1) || items.length;
+                    items.push(item);
+                }
+            };
+        } else {
+            filter = make;
+            var initialItems = count;
+            reset = function () {
+                items = initialItems.slice(0);
+            };
+        }
+        helpers.extend(methods, {
+            getListData: function (request) {
+                request = request || {};
+                var retArr = items.slice(0);
+                request.data = request.data || {};
+                helpers.each((request.data.order || []).slice(0).reverse(), function (name) {
+                    var split = name.split(' ');
+                    retArr = retArr.sort(function (a, b) {
+                        if (split[1].toUpperCase() !== 'ASC') {
+                            if (a[split[0]] < b[split[0]]) {
+                                return 1;
+                            } else if (a[split[0]] === b[split[0]]) {
+                                return 0;
+                            } else {
+                                return -1;
+                            }
+                        } else {
+                            if (a[split[0]] < b[split[0]]) {
+                                return -1;
+                            } else if (a[split[0]] === b[split[0]]) {
+                                return 0;
+                            } else {
+                                return 1;
+                            }
+                        }
+                    });
+                });
+                helpers.each((request.data.group || []).slice(0).reverse(), function (name) {
+                    var split = name.split(' ');
+                    retArr = retArr.sort(function (a, b) {
+                        return a[split[0]] > b[split[0]];
+                    });
+                });
+                var offset = parseInt(request.data.offset, 10) || 0, limit = parseInt(request.data.limit, 10) || items.length - offset, i = 0;
+                for (var param in request.data) {
+                    i = 0;
+                    if (request.data[param] !== undefined && (param.indexOf('Id') !== -1 || param.indexOf('_id') !== -1)) {
+                        while (i < retArr.length) {
+                            if (request.data[param] != retArr[i][param]) {
+                                retArr.splice(i, 1);
+                            } else {
+                                i++;
+                            }
+                        }
+                    }
+                }
+                if (typeof filter === 'function') {
+                    i = 0;
+                    while (i < retArr.length) {
+                        if (!filter(retArr[i], request)) {
+                            retArr.splice(i, 1);
+                        } else {
+                            i++;
+                        }
+                    }
+                } else if (typeof filter === 'object') {
+                    i = 0;
+                    while (i < retArr.length) {
+                        var subset = canSet.subset(retArr[i], request.data, filter);
+                        if (!subset) {
+                            retArr.splice(i, 1);
+                        } else {
+                            i++;
+                        }
+                    }
+                }
+                var responseData = {
+                    'count': retArr.length,
+                    'data': retArr.slice(offset, offset + limit)
+                };
+                helpers.each([
+                    'limit',
+                    'offset'
+                ], function (prop) {
+                    if (prop in request.data) {
+                        responseData[prop] = request.data[prop];
+                    }
+                });
+                return responseData;
+            },
+            getData: function (request, response) {
+                var item = findOne(getId(request));
+                if (typeof item === 'undefined') {
+                    return response(404, 'Requested resource not found');
+                }
+                response(item);
+            },
+            updateData: function (request, response) {
+                var id = getId(request), item = findOne(id);
+                if (typeof item === 'undefined') {
+                    return response(404, 'Requested resource not found');
+                }
+                helpers.extend(item, request.data);
+                response({ id: id }, { location: request.url || '/' + getId(request) });
+            },
+            destroyData: function (request, response) {
+                var id = getId(request), item = findOne(id);
+                if (typeof item === 'undefined') {
+                    return response(404, 'Requested resource not found');
+                }
+                for (var i = 0; i < items.length; i++) {
+                    if (items[i].id == id) {
+                        items.splice(i, 1);
+                        break;
+                    }
+                }
+                return {};
+            },
+            createData: function (settings, response) {
+                var item = typeof make === 'function' ? make(items.length, items) : {};
+                helpers.extend(item, settings.data);
+                if (!item.id) {
+                    item.id = currentId++;
+                }
+                items.push(item);
+                response({ id: item.id }, { location: settings.url + '/' + item.id });
+            }
+        });
+        reset();
+        return helpers.extend({
+            findAll: methods.getListData,
+            findOne: methods.getData,
+            create: methods.createData,
+            update: methods.updateData,
+            destroy: methods.destroyData,
+            getId: getId,
+            find: function (settings) {
+                return findOne(getId(settings));
+            },
+            reset: reset
+        }, methods);
+    };
+});
---
+++ dist/amd/xhr.js
@@ -0,0 +1,186 @@
+/*xhr*/
+define(function (require, exports, module) {
+    var fixtureCore = require('./core');
+    var helpers = require('./helpers/helpers');
+    var deparam = require('./helpers/deparam');
+    var XHR = XMLHttpRequest, GLOBAL = typeof global !== 'undefined' ? global : window;
+    var events = [
+        'abort',
+        'error',
+        'load',
+        'loadend',
+        'loadstart',
+        'progress'
+    ];
+    (function () {
+        var x = new XHR();
+        for (var prop in x) {
+            if (prop.indexOf('on') === 0 && events.indexOf(prop.substr(2)) === -1 && prop !== 'onreadystatechange') {
+                events.push(prop.substr(2));
+            }
+        }
+    }());
+    function callEvents(xhr, ev) {
+        var evs = xhr.__events[ev] || [], fn;
+        for (var i = 0, len = evs.length; i < len; i++) {
+            fn = evs[i];
+            fn.call(xhr);
+        }
+    }
+    var assign = function (dest, source, excluding) {
+        excluding = excluding || {};
+        for (var prop in source) {
+            if (!(prop in XMLHttpRequest.prototype) && !excluding[prop]) {
+                dest[prop] = source[prop];
+            }
+        }
+    };
+    var propsToIgnore = {
+        onreadystatechange: true,
+        onload: true,
+        __events: true
+    };
+    helpers.each(events, function (prop) {
+        propsToIgnore['on' + prop] = true;
+    });
+    var makeXHR = function (mockXHR) {
+        var xhr = new XHR();
+        assign(xhr, mockXHR, propsToIgnore);
+        xhr.onreadystatechange = function (ev) {
+            assign(mockXHR, xhr, propsToIgnore);
+            if (mockXHR.onreadystatechange) {
+                mockXHR.onreadystatechange(ev);
+            }
+        };
+        helpers.each(events, function (eventName) {
+            xhr['on' + eventName] = function () {
+                callEvents(mockXHR, eventName);
+                if (mockXHR['on' + eventName]) {
+                    return mockXHR['on' + eventName].apply(mockXHR, arguments);
+                }
+            };
+        });
+        if (xhr.getResponseHeader) {
+            mockXHR.getResponseHeader = function () {
+                return xhr.getResponseHeader.apply(xhr, arguments);
+            };
+        }
+        if (mockXHR._disableHeaderCheck && xhr.setDisableHeaderCheck) {
+            xhr.setDisableHeaderCheck(true);
+        }
+        return xhr;
+    };
+    GLOBAL.XMLHttpRequest = function () {
+        var headers = this._headers = {};
+        this._xhr = {
+            getAllResponseHeaders: function () {
+                return headers;
+            }
+        };
+        this.__events = {};
+        this.onload = null;
+        this.onerror = null;
+    };
+    helpers.extend(XMLHttpRequest.prototype, {
+        setRequestHeader: function (name, value) {
+            this._headers[name] = value;
+        },
+        open: function (type, url, async) {
+            this.type = type;
+            this.url = url;
+            this.async = async === false ? false : true;
+        },
+        getAllResponseHeaders: function () {
+            return this._xhr.getAllResponseHeaders.apply(this._xhr, arguments);
+        },
+        addEventListener: function (ev, fn) {
+            var evs = this.__events[ev] = this.__events[ev] || [];
+            evs.push(fn);
+        },
+        removeEventListener: function (ev, fn) {
+            var evs = this.__events[ev] = this.__events[ev] || [];
+            var idx = evs.indexOf(fn);
+            if (idx >= 0) {
+                evs.splice(idx, 1);
+            }
+        },
+        setDisableHeaderCheck: function (val) {
+            this._disableHeaderCheck = !!val;
+        },
+        getResponseHeader: function (key) {
+            return '';
+        },
+        send: function (data) {
+            var xhrSettings = {
+                url: this.url,
+                data: data,
+                headers: this._headers,
+                type: this.type.toLowerCase() || 'get',
+                async: this.async
+            };
+            if (!xhrSettings.data && xhrSettings.type === 'get' || xhrSettings.type === 'delete') {
+                xhrSettings.data = deparam(xhrSettings.url.split('?')[1]);
+                xhrSettings.url = xhrSettings.url.split('?')[0];
+            }
+            if (typeof xhrSettings.data === 'string') {
+                try {
+                    xhrSettings.data = JSON.parse(xhrSettings.data);
+                } catch (e) {
+                    xhrSettings.data = deparam(xhrSettings.data);
+                }
+            }
+            var fixtureSettings = fixtureCore.get(xhrSettings);
+            if (fixtureSettings && typeof fixtureSettings.fixture === 'function') {
+                var mockXHR = this;
+                return fixtureCore.callDynamicFixture(xhrSettings, fixtureSettings, function (status, body, headers, statusText) {
+                    body = typeof body === 'string' ? body : JSON.stringify(body);
+                    helpers.extend(mockXHR, {
+                        readyState: 4,
+                        status: status
+                    });
+                    var success = status >= 200 && status < 300 || status === 304;
+                    if (success) {
+                        helpers.extend(mockXHR, {
+                            statusText: statusText || 'OK',
+                            responseText: body
+                        });
+                    } else {
+                        helpers.extend(mockXHR, {
+                            statusText: statusText || 'error',
+                            responseText: body
+                        });
+                    }
+                    if (mockXHR.onreadystatechange) {
+                        mockXHR.onreadystatechange({ target: mockXHR });
+                    }
+                    callEvents(mockXHR, 'progress');
+                    if (mockXHR.onprogress) {
+                        mockXHR.onprogress();
+                    }
+                    if (success) {
+                        callEvents(mockXHR, 'load');
+                        if (mockXHR.onload) {
+                            mockXHR.onload();
+                        }
+                    } else {
+                        callEvents(mockXHR, 'error');
+                        if (mockXHR.onerror) {
+                            mockXHR.onerror();
+                        }
+                    }
+                    callEvents(mockXHR, 'loadend');
+                    if (mockXHR.onloadend) {
+                        mockXHR.onloadend();
+                    }
+                });
+            }
+            var xhr = makeXHR(this);
+            if (fixtureSettings) {
+                helpers.extend(xhr, fixtureSettings);
+            }
+            this._xhr = xhr;
+            xhr.open(xhr.type, xhr.url, xhr.async);
+            return xhr.send(data);
+        }
+    });
+});
