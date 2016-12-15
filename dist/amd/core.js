c7d70a1ace64b34dbb74cf0cef175a787ddcb1a5
Kevin Phillips <kphillips86@gmail.com>
2016-12-15 09:09:07 -0600

    1.0.11

---
+++ dist/amd/core.js
@@ -0,0 +1,203 @@
+/*can-fixture@1.0.10#core*/
+define(function (require, exports, module) {
+    var canSet = require('can-set');
+    var sub = require('can-util/js/string').sub;
+    var each = require('can-util/js/each');
+    var assign = require('can-util/js/assign');
+    var isEmptyObject = require('can-util/js/is-empty-object');
+    require('./store');
+    var fixtures = [];
+    exports.fixtures = fixtures;
+    exports.add = function (settings, fixture) {
+        if (fixture && (fixture.getData || fixture.getListData)) {
+            var root = settings, store = fixture, idProp = store.idProp, itemRegex = new RegExp('\\/\\{' + idProp + '\\}.*'), rootIsItemUrl = itemRegex.test(root), getListUrl = rootIsItemUrl ? root.replace(itemRegex, '') : root, getItemUrl = rootIsItemUrl ? root : root.trim() + '/{' + idProp + '}';
+            fixture = undefined;
+            settings = {};
+            settings['GET ' + getItemUrl] = store.getData;
+            settings['DELETE ' + getItemUrl] = store.destroyData;
+            settings['PUT ' + getItemUrl] = store.updateData;
+            settings['GET ' + getListUrl] = store.getListData;
+            settings['POST ' + getListUrl] = store.createData;
+        }
+        if (fixture !== undefined) {
+            if (typeof settings === 'string') {
+                var matches = settings.match(/(GET|POST|PUT|DELETE|PATCH) (.+)/i);
+                if (!matches) {
+                    settings = { url: settings };
+                } else {
+                    settings = {
+                        url: matches[2],
+                        type: matches[1]
+                    };
+                }
+            }
+            var index = exports.index(settings, true);
+            if (index > -1) {
+                fixtures.splice(index, 1);
+            }
+            if (fixture == null) {
+                return;
+            }
+            if (typeof fixture === 'object') {
+                var data = fixture;
+                fixture = function () {
+                    return data;
+                };
+            }
+            settings.fixture = fixture;
+            fixtures.unshift(settings);
+        } else {
+            each(settings, function (fixture, url) {
+                exports.add(url, fixture);
+            });
+        }
+    };
+    var $fixture = exports.add;
+    $fixture.on = true;
+    $fixture.delay = 10;
+    exports.callDynamicFixture = function (xhrSettings, fixtureSettings, cb) {
+        xhrSettings.data = fixtureSettings.data;
+        var response = function () {
+            var res = exports.extractResponse.apply(xhrSettings, arguments);
+            return cb.apply(this, res);
+        };
+        var callFixture = function () {
+            var result = fixtureSettings.fixture(xhrSettings, response, xhrSettings.headers, fixtureSettings);
+            if (result !== undefined) {
+                response(200, result);
+            }
+        };
+        if (!xhrSettings.async) {
+            callFixture();
+            return null;
+        } else {
+            return setTimeout(callFixture, $fixture.delay);
+        }
+    };
+    exports.index = function (settings, exact) {
+        for (var i = 0; i < fixtures.length; i++) {
+            if (exports.matches(settings, fixtures[i], exact)) {
+                return i;
+            }
+        }
+        return -1;
+    };
+    exports.get = function (xhrSettings) {
+        if (!$fixture.on) {
+            return;
+        }
+        var index = exports.index(xhrSettings, true);
+        if (index === -1) {
+            index = exports.index(xhrSettings, false);
+        }
+        var fixtureSettings = index >= 0 ? assign({}, fixtures[index]) : undefined;
+        if (fixtureSettings) {
+            var url = fixtureSettings.fixture, data = exports.dataFromUrl(fixtureSettings.url, xhrSettings.url);
+            if (typeof fixtureSettings.fixture === 'string') {
+                if (data) {
+                    url = sub(url, data);
+                }
+                fixtureSettings.url = url;
+                fixtureSettings.data = null;
+                fixtureSettings.type = 'GET';
+                if (!fixtureSettings.error) {
+                    fixtureSettings.error = function (xhr, error, message) {
+                        throw 'fixtures.js Error ' + error + ' ' + message;
+                    };
+                }
+            } else {
+                var xhrData = assign({}, xhrSettings.data || {});
+                fixtureSettings.data = assign(xhrData, data);
+            }
+        }
+        return fixtureSettings;
+    };
+    exports.matches = function (settings, fixture, exact) {
+        if (exact) {
+            return canSet.equal(settings, fixture, {
+                fixture: function () {
+                    return true;
+                }
+            });
+        } else {
+            return canSet.subset(settings, fixture, exports.defaultCompare);
+        }
+    };
+    var isEmptyOrNull = function (a, b) {
+        if (a == null && isEmptyObject(b)) {
+            return true;
+        } else if (b == null && isEmptyObject(a)) {
+            return true;
+        } else {
+            return canSet.equal(a, b);
+        }
+    };
+    var isEmptyOrSubset = function (a, b) {
+        if (a == null && isEmptyObject(b)) {
+            return true;
+        } else if (b == null && isEmptyObject(a)) {
+            return true;
+        } else {
+            return canSet.subset(a, b);
+        }
+    };
+    exports.defaultCompare = {
+        url: function (a, b) {
+            return !!exports.dataFromUrl(b, a);
+        },
+        fixture: function () {
+            return true;
+        },
+        xhr: function () {
+            return true;
+        },
+        type: function (a, b) {
+            return b && a ? a.toLowerCase() === b.toLowerCase() : b === a;
+        },
+        method: function (a, b) {
+            return b && a ? a.toLowerCase() === b.toLowerCase() : b === a;
+        },
+        helpers: function () {
+            return true;
+        },
+        headers: isEmptyOrNull,
+        data: isEmptyOrSubset
+    };
+    var replacer = /\{([^\}]+)\}/g;
+    exports.dataFromUrl = function (fixtureUrl, url) {
+        if (!fixtureUrl) {
+            return {};
+        }
+        var order = [], fixtureUrlAdjusted = fixtureUrl.replace('.', '\\.').replace('?', '\\?'), res = new RegExp(fixtureUrlAdjusted.replace(replacer, function (whole, part) {
+                order.push(part);
+                return '([^/]+)';
+            }) + '$').exec(url), data = {};
+        if (!res) {
+            return null;
+        }
+        res.shift();
+        each(order, function (name) {
+            data[name] = res.shift();
+        });
+        return data;
+    };
+    exports.extractResponse = function (status, response, headers, statusText) {
+        if (typeof status !== 'number') {
+            headers = response;
+            response = status;
+            status = 200;
+        }
+        if (typeof headers === 'string') {
+            statusText = headers;
+            headers = {};
+        }
+        return [
+            status,
+            response,
+            headers,
+            statusText
+        ];
+    };
+    exports.log = function () {
+    };
+});
---
+++ dist/amd/helpers/legacyStore.js
@@ -0,0 +1,182 @@
+/*can-fixture@1.0.10#helpers/legacyStore*/
+define(function (require, exports, module) {
+    var getId = require('./getid');
+    var canSet = require('can-set');
+    var isArrayLike = require('can-util/js/is-array-like');
+    var each = require('can-util/js/each');
+    var assign = require('can-util/js/assign');
+    module.exports = function (count, make, filter) {
+        var currentId = 0, items, findOne = function (id) {
+                for (var i = 0; i < items.length; i++) {
+                    if (id == items[i].id) {
+                        return items[i];
+                    }
+                }
+            }, methods = {}, types, reset;
+        if (isArrayLike(count) && typeof count[0] === 'string') {
+            types = count;
+            count = make;
+            make = filter;
+            filter = arguments[3];
+        } else if (typeof count === 'string') {
+            types = [
+                count + 's',
+                count
+            ];
+            count = make;
+            make = filter;
+            filter = arguments[3];
+        }
+        if (typeof count === 'number') {
+            items = [];
+            reset = function () {
+                items = [];
+                for (var i = 0; i < count; i++) {
+                    var item = make(i, items);
+                    if (!item.id) {
+                        item.id = i;
+                    }
+                    currentId = Math.max(item.id + 1, currentId + 1) || items.length;
+                    items.push(item);
+                }
+            };
+        } else {
+            filter = make;
+            var initialItems = count;
+            reset = function () {
+                items = initialItems.slice(0);
+            };
+        }
+        assign(methods, {
+            getListData: function (request) {
+                request = request || {};
+                var retArr = items.slice(0);
+                request.data = request.data || {};
+                each((request.data.order || []).slice(0).reverse(), function (name) {
+                    var split = name.split(' ');
+                    retArr = retArr.sort(function (a, b) {
+                        if (split[1].toUpperCase() !== 'ASC') {
+                            if (a[split[0]] < b[split[0]]) {
+                                return 1;
+                            } else if (a[split[0]] === b[split[0]]) {
+                                return 0;
+                            } else {
+                                return -1;
+                            }
+                        } else {
+                            if (a[split[0]] < b[split[0]]) {
+                                return -1;
+                            } else if (a[split[0]] === b[split[0]]) {
+                                return 0;
+                            } else {
+                                return 1;
+                            }
+                        }
+                    });
+                });
+                each((request.data.group || []).slice(0).reverse(), function (name) {
+                    var split = name.split(' ');
+                    retArr = retArr.sort(function (a, b) {
+                        return a[split[0]] > b[split[0]];
+                    });
+                });
+                var offset = parseInt(request.data.offset, 10) || 0, limit = parseInt(request.data.limit, 10) || items.length - offset, i = 0;
+                for (var param in request.data) {
+                    i = 0;
+                    if (request.data[param] !== undefined && (param.indexOf('Id') !== -1 || param.indexOf('_id') !== -1)) {
+                        while (i < retArr.length) {
+                            if (request.data[param] != retArr[i][param]) {
+                                retArr.splice(i, 1);
+                            } else {
+                                i++;
+                            }
+                        }
+                    }
+                }
+                if (typeof filter === 'function') {
+                    i = 0;
+                    while (i < retArr.length) {
+                        if (!filter(retArr[i], request)) {
+                            retArr.splice(i, 1);
+                        } else {
+                            i++;
+                        }
+                    }
+                } else if (typeof filter === 'object') {
+                    i = 0;
+                    while (i < retArr.length) {
+                        var subset = canSet.subset(retArr[i], request.data, filter);
+                        if (!subset) {
+                            retArr.splice(i, 1);
+                        } else {
+                            i++;
+                        }
+                    }
+                }
+                var responseData = {
+                    'count': retArr.length,
+                    'data': retArr.slice(offset, offset + limit)
+                };
+                each([
+                    'limit',
+                    'offset'
+                ], function (prop) {
+                    if (prop in request.data) {
+                        responseData[prop] = request.data[prop];
+                    }
+                });
+                return responseData;
+            },
+            getData: function (request, response) {
+                var item = findOne(getId(request));
+                if (typeof item === 'undefined') {
+                    return response(404, 'Requested resource not found');
+                }
+                response(item);
+            },
+            updateData: function (request, response) {
+                var id = getId(request), item = findOne(id);
+                if (typeof item === 'undefined') {
+                    return response(404, 'Requested resource not found');
+                }
+                assign(item, request.data);
+                response({ id: id }, { location: request.url || '/' + getId(request) });
+            },
+            destroyData: function (request, response) {
+                var id = getId(request), item = findOne(id);
+                if (typeof item === 'undefined') {
+                    return response(404, 'Requested resource not found');
+                }
+                for (var i = 0; i < items.length; i++) {
+                    if (items[i].id == id) {
+                        items.splice(i, 1);
+                        break;
+                    }
+                }
+                return {};
+            },
+            createData: function (settings, response) {
+                var item = typeof make === 'function' ? make(items.length, items) : {};
+                assign(item, settings.data);
+                if (!item.id) {
+                    item.id = currentId++;
+                }
+                items.push(item);
+                response({ id: item.id }, { location: settings.url + '/' + item.id });
+            }
+        });
+        reset();
+        return assign({
+            findAll: methods.getListData,
+            findOne: methods.getData,
+            create: methods.createData,
+            update: methods.updateData,
+            destroy: methods.destroyData,
+            getId: getId,
+            find: function (settings) {
+                return findOne(getId(settings));
+            },
+            reset: reset
+        }, methods);
+    };
+});
